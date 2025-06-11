7ef2992cf94cda9bff60011afbc02fae08ee5778
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2025-06-11 02:17:43 -0400

    Backend diseño OK
    Rutas sin terminar

---
+++ back/rutas/BaseDatos/UsarBaseDatos.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+const pool = require('../../config/DB');
+
+JWT_SECRET = process.env.JWT_SECRET
+
+
+const { authMiddleware, Verifica } = require('../../middleware/TipoUsuario.js');
+
+
+router.post('/EjecutarQWERY', authMiddleware, Verifica("usuario"), (req, res) => {
+
+    console.log("Usuario autenticado:", req.user.id);
+    console.log("Rol del usuario:", req.user.rol);
+
+    res.json({ message: 'Acceso permitido a la ruta protegida', usuario: req.user });
+});
+
+
+router.get('/ObtenerTablas', authMiddleware, Verifica("usuario"), (req, res) => {
+
+    console.log("Usuario autenticado:", req.user.id);
+    console.log("Rol del usuario:", req.user.rol);
+
+    res.json({ message: 'Acceso permitido a la ruta protegida', usuario: req.user });
+});
+
+
+router.get('/ObtenerTabla/:nombre', authMiddleware, Verifica("usuario"), (req, res) => {
+
+    console.log("Usuario autenticado:", req.user.id);
+    console.log("Rol del usuario:", req.user.rol);
+
+    res.json({ message: 'Acceso permitido a la ruta protegida', usuario: req.user });
+});
+
---
+++ back/rutas/Ejercicios/CrearEjercicio.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
+const pool = require('../../config/DB');
+
+JWT_SECRET = process.env.JWT_SECRET
+
+
+const { authMiddleware, Verifica } = require('../../middleware/TipoUsuario.js');
+
+
+router.post('/CrearEjercicio', authMiddleware, Verifica("usuario"), (req, res) => {
+
+    console.log("Usuario autenticado:", req.user.id);
+    console.log("Rol del usuario:", req.user.rol);
+
+    res.json({ message: 'Acceso permitido a la ruta protegida', usuario: req.user });
+});
+
+
+router.get('/ObtenerEjercicios', authMiddleware, Verifica("usuario"), (req, res) => {
+
+    console.log("Usuario autenticado:", req.user.id);
+    console.log("Rol del usuario:", req.user.rol);
+
+    res.json({ message: 'Acceso permitido a la ruta protegida', usuario: req.user });
+});
+
+
+router.get('/ObtenerEjercicio/:id', authMiddleware, Verifica("usuario"), (req, res) => {
+
+    console.log("Usuario autenticado:", req.user.id);
+    console.log("Rol del usuario:", req.user.rol);
+
+    res.json({ message: 'Acceso permitido a la ruta protegida', usuario: req.user });
+});
+
+
+router.put('/editarEjercicio', authMiddleware, Verifica("usuario"), (req, res) => {
+
+    console.log("Usuario autenticado:", req.user.id);
+    console.log("Rol del usuario:", req.user.rol);
+
+    res.json({ message: 'Acceso permitido a la ruta protegida', usuario: req.user });
+});
+
+
+router.delete('/BorrarEjercicio', authMiddleware, Verifica("usuario"), (req, res) => {
+
+    console.log("Usuario autenticado:", req.user.id);
+    console.log("Rol del usuario:", req.user.rol);
+
+    res.json({ message: 'Acceso permitido a la ruta protegida', usuario: req.user });
+});
+
